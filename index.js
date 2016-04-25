cfbc7e8935f0936ea0ecadd7239b7e5c37d43b75
Alexander Kleinhans <zndr.k.94@gmail.com>
2016-04-25 10:57:24 -0600

    Added 404 page.

--- index.js
+++ index.js
@@ -11,21 +11,25 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+	response.render('pages/index')
 });
 
 app.get('/contact', function(request, response) {
-  response.render('pages/contact')
+	response.render('pages/contact')
 });
 
 app.get('/resume', function(request, response) {
-  response.render('pages/resume')
+	response.render('pages/resume')
 });
 
 app.get('/portfolio', function(request, response) {
-  response.render('pages/portfolio')
+	response.render('pages/portfolio')
+});
+
+app.get('*', function(request, response) {
+	response.render('pages/404')
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
